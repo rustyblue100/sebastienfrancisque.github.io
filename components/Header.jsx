0c68d6ca1eeb361f917619baf9be4ef5dd5dd1e9
Sebastien Francisque <sebastienfrancisque@seb-mac-laptop.local>
2022-04-09 16:36:11 -0400

    styled: name title mobile size

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,6 +3,10 @@ import styled from "styled-components";
 const Navbar = styled.div`
   margin-top: 20px;
   font-family: "Cutive Mono", monospace;
+
+  @media (max-width: 660px) {
+    font-size: 0.7rem;
+  }
 `;
 
 const Header = () => {
